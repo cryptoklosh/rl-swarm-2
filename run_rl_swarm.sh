ce37ed73578842958d63f22ccd7eb39e9e38bf6b
Petr Stroganov <petr.stroganov@gmail.com>
2025-04-16 00:26:35 +0900

    Fix

--- run_rl_swarm.sh
+++ run_rl_swarm.sh
@@ -198,6 +198,7 @@ if [ "$CONNECT_TO_TESTNET" = true ]; then
     echo "Waiting for API key to become activated..."
     while true; do
         STATUS=$(curl -s "http://localhost:3000/api/get-api-key-status?orgId=$ORG_ID")
+        echo "API key status: $STATUS"
         if [[ "$STATUS" == "activated" ]]; then
             echo "API key is activated! Proceeding..."
             break
