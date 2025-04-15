be5c3f58568edd58d44b1b3bf5bd9ebdc4365920
Petr Stroganov <petr.stroganov@gmail.com>
2025-04-15 14:00:49 +0900

    Fix

--- modal-login/app/layout.tsx
+++ modal-login/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   description: "Modal sign in for Gensyn Testnet",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
