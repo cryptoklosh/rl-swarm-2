993b679e7b03d7d9f7181598e166a208f8dee81d
Petr Stroganov <petr.stroganov@gmail.com>
2025-04-20 15:15:07 +0900

    Fix

--- hivemind_exp/trainer/hivemind_grpo_trainer.py
+++ hivemind_exp/trainer/hivemind_grpo_trainer.py
@@ -1,6 +1,7 @@
 import gc
 import hashlib
 import logging
+from pathlib import Path
 import time
 from typing import Any
 
@@ -182,6 +183,7 @@ class HivemindGRPOTrainer:
             self.logger.info(
                 f"📉 Finished training round: {round_num} stage: {stage_num}"
             )
+            Path("/root/healthy.ok").touch()
 
         # Push to HF hub if desired
         # TODO: Come back and add additional logic checking if they've provided access token+HF username
