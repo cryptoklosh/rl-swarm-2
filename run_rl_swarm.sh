d5511f3f9e6a343d84faba27e7e337e5a18f2055
Petr Stroganov <petr.stroganov@gmail.com>
2025-05-03 12:35:55 +0900

    Fix

--- run_rl_swarm.sh
+++ run_rl_swarm.sh
@@ -220,7 +220,7 @@ curl -s "http://localhost:3000" > /dev/null
 
 cd ..
 
-if [ ! -f "${IDENTITY_PATH}"]; then
+if [ ! -f "${IDENTITY_PATH}" ]; then
     echo_green ">> Waiting for modal userData.json to be created..."
     while [ ! -f "modal-login/temp-data/userData.json" ]; do
         sleep 5  # Wait for 5 seconds before checking again
