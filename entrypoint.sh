ec164d25b412b86ddcc676a8ee5223c77da3a769
Petr Stroganov <petr.stroganov@gmail.com>
2025-05-04 16:42:50 +0900

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /root/ssl
-openssl req -x509 -newkey rsa:4096 -keyout /root/ssl/key.pem -out /root/ssl/cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=NodesGarden/L=NodesGarden/O=NodesGarden/OU=NodesGarden/CN=$EXTERNAL_HOST_IP"
+openssl req -x509 -newkey rsa:4096 -keyout /root/ssl/key.pem -out /root/ssl/cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=NodesGarden/L=NodesGarden/O=NodesGarden/OU=NodesGarden/CN=$HOST_IP"
 
 # python3 -m venv .venv
 # source .venv/bin/activate
