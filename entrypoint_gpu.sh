0291bc0cf91fc8d85072092db2a0e9c50b8ef8e5
Petr Stroganov <petr.stroganov@gmail.com>
2025-05-25 15:09:04 +0900

    Fix

---
+++ entrypoint_gpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# mkdir -p /root/ssl
+# openssl req -x509 -newkey rsa:4096 -keyout /root/ssl/key.pem -out /root/ssl/cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=NodesGarden/L=NodesGarden/O=NodesGarden/OU=NodesGarden/CN=$HOST_IP"
+
+python3 -m venv .venv
+source .venv/bin/activate
+./run_rl_swarm_gpu.sh
