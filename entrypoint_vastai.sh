04b2e1d544273a853ebe06dccba84776241778aa
Petr Stroganov <petr.stroganov@gmail.com>
2025-05-29 00:02:48 +0900

    Fix

--- entrypoint_vastai.sh
+++ entrypoint_vastai.sh
@@ -3,8 +3,8 @@
 # mkdir -p /root/ssl
 # openssl req -x509 -newkey rsa:4096 -keyout /root/ssl/key.pem -out /root/ssl/cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=NodesGarden/L=NodesGarden/O=NodesGarden/OU=NodesGarden/CN=$HOST_IP"
 
-# python3 -m venv .venv
-# source .venv/bin/activate
+python3 -m venv .venv
+source .venv/bin/activate
 
 function run_node_manager() {
     MANIFEST_FILE=/root/node-manager/nodeV3.yaml \
